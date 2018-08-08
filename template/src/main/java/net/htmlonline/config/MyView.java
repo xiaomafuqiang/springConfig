9339ce1ec7f83616e4efce1afb9cc536e8385950
mafuqiang <mafuqiang@outlook.com>
2018-08-08 14:49:53 +0800

    BeanNameViewResolver -->

---
+++ template/src/main/java/net/htmlonline/config/MyView.java
@@ -0,0 +1,21 @@
+package net.htmlonline.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component("aaa")
+public class MyView implements View {
+    @Override
+    public String getContentType() {
+        return "text/html";
+    }
+
+    @Override
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.getWriter().write("hello this is my view" + this.getClass().getSimpleName());
+    }
+}
